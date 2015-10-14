6d7759c38722128fcfc7e726b58be39105e2b4ad
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2015-10-14 11:54:21 +0200

    Working on the wrapper for FALCON. Added a function to convert FASTQ headers to PacBio format if this has not been done before. Created a better logging system which also stores logs to a text file in the output folder.

--- src/wrappers/wrapper_falcon.py
+++ src/wrappers/wrapper_falcon.py
@@ -6,6 +6,8 @@ SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 import sys
 sys.path.append(SCRIPT_PATH + '/../')
 
+import re;
+
 import subprocess
 import multiprocessing
 import getpass
@@ -32,14 +34,27 @@ CREATE_OUTPUT_FOLDER = True
 # DRY_RUN = True;
 DRY_RUN = False;
 
-def execute_command(command, dry_run=True):
+### Logs messages to STDERR and an output log file if provided (opened elsewhere).
+def log(message, fp_log):
+    timestamp = strftime("%Y/%m/%d %H:%M:%S", gmtime());
+
+    sys.stderr.write('[%s wrapper %s] %s\n' % (ASSEMBLER_NAME, timestamp, message))
+    sys.stderr.flush();
+    if (fp_log != None):
+        fp_log.write('[%s wrapper %s] %s\n' % (ASSEMBLER_NAME, timestamp, message))
+        fp_log.flush();
+
+def execute_command(command, fp_log, dry_run=True):
     if (dry_run == True):
         # sys.stderr.write('Warning: dry_run == True\n');
-        sys.stderr.write('[%s wrapper] Executing (dryrun): "%s"\n' % (ASSEMBLER_NAME, command));
+#        sys.stderr.write('[%s wrapper] Executing (dryrun): "%s"\n' % (ASSEMBLER_NAME, command));
+        log('Executing (dryrun): "%s".' % (command), fp_log);
     if (dry_run == False):
-        sys.stderr.write('[%s wrapper] Executing: "%s"\n' % (ASSEMBLER_NAME, command));
+        # sys.stderr.write('[%s wrapper] Executing: "%s"\n' % (ASSEMBLER_NAME, command));
+        log('Executing: "%s".' % (command), fp_log);
         subprocess.call(command, shell=True);
-    sys.stderr.write('\n');
+    # sys.stderr.write('\n');
+    log('\n', fp_log);
 
 def measure_command(measure_file):
     if (MODULE_BASICDEFINES == True):
@@ -48,6 +63,117 @@ def measure_command(measure_file):
         sys.stderr.write('ERROR: Cgmemtime tool not found! Exiting.\n');
         exit(1);
         # return '/usr/bin/time --format "Command line: %%C\\nReal time: %%e s\\nCPU time: -1.0 s\\nUser time: %%U s\\nSystem time: %%S s\\nMaximum RSS: %%M kB\\nExit status: %%x" --quiet -o %s ' % measure_file;
+
+def peek(fp, num_chars):
+    data = fp.read(num_chars);
+    if len(data) == 0:
+        return '';
+    fp.seek(num_chars * -1, 1);
+    return data;
+
+# Returns a single read from the given FASTA/FASTQ file.
+# Parameter header contains only the header of the read.
+# Parameter lines contains all lines of the read, which include:
+# - header
+# - seq
+# - '+' if FASTQ
+# - quals if FASTQ
+# Parameter lines is an array of strings, each for one component.
+# Please note that multiline FASTA/FASTQ entries (e.g. sequence line)
+# will be truncated into one single line.
+def get_single_read(fp):
+    lines = [];
+    
+    line = fp.readline();
+    header = line.rstrip();
+    header_leading_char = '';
+    if (len(header) > 0):
+        sequence_separator = header[0];
+        header_leading_char = header[0];
+        header = header[1:];            # Strip the '>' or '@' sign from the beginning.
+    else:
+        return ['', []];
+    
+    next_char = peek(fp, 1);
+    
+    line_string = '';
+    lines.append(header_leading_char + header);
+    
+    num_lines = 1;
+    #while len(next_char) > 0 and next_char != sequence_separator or (next_char == '@' and num_lines < 4):
+    while (len(next_char) > 0 and (next_char != sequence_separator or (next_char == '@' and num_lines < 4))):
+        line = fp.readline();
+        if (line.rstrip() == '+' or line.rstrip() == ('+' + header)):
+        #if (line.rstrip()[0] == '+'):
+            lines.append(line_string);
+            lines.append(line.rstrip());
+            line_string = '';
+        else:
+            line_string += line.rstrip();
+        next_char = peek(fp, 1);
+        num_lines += 1;
+        
+    lines.append(line_string);
+    
+    return [header, lines];
+
+### DALIGNER requires PacBio format of headers in reads
+### Parameter read_count_offset specifies the ID of the first read in the file.
+### For every read, this ID gets updated.
+### This ID is given as the first added parameter to the PacBio header.
+### The new header will be formatted as: old_header/read_id/0_readlen .
+def convert_reads_to_pacbio_format(reads_file, out_reads_file, fp_log, read_count_offset=0):
+    try:
+        fp_in = open(reads_file, 'r');
+    except:
+        log('ERROR: Could not open file "%s" for reading! Exiting.\n' % (reads_file), fp_log);
+        exit(0);
+
+    try:
+        fp_out = open(out_reads_file, 'w');
+    except:
+        log('ERROR: Could not open file "%s" for writing! Exiting.\n' % (out_reads_file), fp_log);
+        exit(0);
+
+    current_read = read_count_offset;
+    header_conversion_hash = {};
+
+    while True:
+        [header, read] = get_single_read(fp_in);
+        
+        if (len(read) == 0):
+            break;
+
+        current_read += 1;
+
+        if (len(read[1]) <= 20):    ### DALIGNER has a lower length limit of 10bp.
+            log('Found a read shorter than 20bp. Removing from the output.\n', fp_log);
+            continue;
+
+        ### Check if the read is already formatted like PacBio.
+        if (re.match('^(.*?)/([\d]+)/([\d]+)_([\d]+)$', header) != None):
+            continue;
+
+        ### Take only the part of the header up to the first whitespace, and replace all existing '/' with '_' to reduce chances of
+        ### incompatibility with weird tools.
+        trimmed_header = re.sub('[^0-9a-zA-Z]', '_', header.split()[0]); # re.sub("[|:", "_", read[0][1:]);
+
+        # pacbio_header = '%s/%d/0_%d RQ=0.850' % (trimmed_header, current_read, len(read[1]));
+        pacbio_header = '%s/%d/0_%d' % (trimmed_header, current_read, len(read[1]));
+        header_conversion_hash[pacbio_header] = header;
+        read[0] = '%s%s' % (read[0][0], pacbio_header); ### Keep the first char of the header line.
+        read[1] = re.sub("(.{500})", "\\1\n", read[1], 0, re.DOTALL);   ### Wrap the sequence line, because DALIGNER has a 9998bp line len limit.
+        if (len(read) == 4):
+            read[3] = re.sub("(.{500})", "\\1\n", read[3], 0, re.DOTALL);   ### Wrap the qual line, because DALIGNER has a 9998bp line len limit.
+        fp_out.write('\n'.join(read) + '\n');
+
+    log('\n', fp_log);
+    fp_in.close();
+    fp_out.close();
+
+    last_read_offset = current_read;
+
+    return [header_conversion_hash, last_read_offset];
 
 
 
@@ -63,32 +189,61 @@ def run(reads_files, reference_file, machine_name, output_path, output_suffix=''
     ### Reference for running a local job instead of using an SGE cluster:
     ### http://seqanswers.com/forums/showthread.php?t=50937
 
-    sys.stderr.write('[%s wrapper] Running assembly using %s.\n' % (ASSEMBLER_NAME, ASSEMBLER_NAME))
-
     reference_file = os.path.abspath(reference_file);
     output_path = os.path.abspath(output_path);
 
+    ### Backup old assembly results, and create the new output folder.
     if (os.path.exists(output_path)):
         timestamp = strftime("%Y_%m_%d-%H_%M_%S", gmtime());
         os.rename(output_path, '%s.bak_%s' % (output_path, timestamp));
-
     if (not os.path.exists(output_path)):
-        sys.stderr.write('[%s wrapper] Creating a directory on path "%s".\n' % (ASSEMBLER_NAME, output_path))
+        log('Creating a directory on path "%s".' % (output_path), None);
         os.makedirs(output_path);
+
+    log_file = '%s/wrapper_log.txt' % (output_path);
+    try:
+        fp_log = open(log_file, 'a');
+    except Exception, e:
+        log('ERROR: Could not open file "%s" for writing! Using only STDERR for logging.' % (log_file), None);
+        # sys.stderr.write(str(e) + '\n');
+        fp_log = None;
+
+    # sys.stderr.write('[%s wrapper] Running assembly using %s.\n' % (ASSEMBLER_NAME, ASSEMBLER_NAME))
+    log('Running assembly using %s.' % (ASSEMBLER_NAME), fp_log);
+
+    ### Prepare a holder for all reads paths.
+    fofn_paths = [];
+
+    ### Convert read headers to PacBio format so that DALIGNER can process them.
+    # sys.stderr.write('[%s wrapper] Converting reads files:\n' % (ASSEMBLER_NAME))
+    log('Converting reads files:', fp_log);
+    last_read_offset = 0;
+    all_header_conversion_hash = {};
+    i = 0;
+    for reads_file in reads_files:
+        i += 1;
+        reads_file = os.path.abspath(reads_file);
+        out_reads_file = '%s.pbheader%s' % (os.path.splitext(reads_file)[0], os.path.splitext(reads_file)[1]);
+        # sys.stderr.write('[%s wrapper] \t(%d) %s -> %s\n' % (ASSEMBLER_NAME, i, reads_file, out_reads_file))
+        log('\t(%d) %s -> %s' % (i, reads_file, out_reads_file), fp_log);
+        fofn_paths.append(out_reads_file);
+        if (DRY_RUN == False):
+            [header_conversion_hash, last_read_offset] = convert_reads_to_pacbio_format(reads_file, out_reads_file, fp_log, read_count_offset=last_read_offset);
+            all_header_conversion_hash.update(header_conversion_hash);
 
     ### Create a .fofn file containing paths to all reads files.
     fofn_file = '%s/input.fofn' % (output_path);
     try:
         fp_fofn = open(fofn_file, 'w');
     except Exception, e:
-        sys.stderr.write('ERROR: Could not open file "%s" writing!\n' % (fofn_file));
+        log('ERROR: Could not open file "%s" writing!\n' % (fofn_file), fp_log);
         return;
-    sys.stderr.write('[%s wrapper] All reads files:\n' % (ASSEMBLER_NAME))
-    i = 0;
-    for reads_file in reads_files:
-        i += 1;
-        fp_fofn.write(os.path.abspath(reads_file) + '\n');
-        sys.stderr.write('[%s wrapper] \t(%d) %s\n' % (ASSEMBLER_NAME, i, reads_file))
+    fp_fofn.write('\n'.join(fofn_paths) + '\n');
+    # i = 0;
+    # for reads_file in reads_files:
+    #     i += 1;
+    #     fp_fofn.write(os.path.abspath(reads_file) + '\n');
+    #     sys.stderr.write('[%s wrapper] \t(%d) %s\n' % (ASSEMBLER_NAME, i, reads_file))
     fp_fofn.close();
 
     ### Create a config file for the assembly.
@@ -132,14 +287,14 @@ def run(reads_files, reference_file, machine_name, output_path, output_suffix=''
         cfg_lines += '\n';
         cfg_lines += '# Running a PacBio reads configuration.\n';
     else:
-        sys.stderr.write('ERROR: Unknown machine name: "%s". Skipping assembly.\n' % (machine_name));
+        log('ERROR: Unknown machine name: "%s". Skipping assembly.\n' % (machine_name), fp_log);
         return;
 
     cfg_file = '%s/fc_run.cfg' % (output_path);
     try:
         fp_cfg = open(cfg_file, 'w');
     except Exception, e:
-        sys.stderr.write('ERROR: Could not open file "%s" writing!\n' % (cfg_file));
+        log('ERROR: Could not open file "%s" writing!\n' % (cfg_file), fp_log);
         return;
     fp_cfg.write(cfg_lines);
     fp_cfg.close();
@@ -148,7 +303,7 @@ def run(reads_files, reference_file, machine_name, output_path, output_suffix=''
 
     if (MODULE_BASICDEFINES == True):
         command = 'sudo %s/cgmemtime/cgmemtime --setup -g %s --perm 775' % (basicdefines.TOOLS_ROOT, getpass.getuser());
-        execute_command(command, dry_run=DRY_RUN);
+        execute_command(command, fp_log, dry_run=DRY_RUN);
 
     memtime_file = '%s/%s.memtime' % (output_path, ASSEMBLER_NAME);
     FC_path = '%s/fc_env' % (ASSEMBLER_PATH);
@@ -157,7 +312,10 @@ def run(reads_files, reference_file, machine_name, output_path, output_suffix=''
     setup_commands.append('cd %s' % (output_path));
     setup_commands.append('%s fc_run.py %s' % (measure_command(memtime_file), cfg_file));
     command = '; '.join(setup_commands);
-    execute_command(command, dry_run=DRY_RUN);
+    execute_command(command, fp_log, dry_run=DRY_RUN);
+
+    if (fp_log != None):
+        fp_log.close();
 
 
 
@@ -179,15 +337,19 @@ def get_memtime():
 # root privileges.
 def download_and_install():
     if os.path.exists(ASSEMBLER_BIN):
-        sys.stderr.write('[%s wrapper] Bin found at %s. Skipping installation.\n' % (ASSEMBLER_NAME, ASSEMBLER_BIN))
+        # sys.stderr.write('[%s wrapper] Bin found at %s. Skipping installation.\n' % (ASSEMBLER_NAME, ASSEMBLER_BIN))
+        log('Bin found at %s. Skipping installation.' % (ASSEMBLER_BIN), None);
+
     else:
-        sys.stderr.write('[%s wrapper] Started installation of %s.\n' % (ASSEMBLER_NAME, ASSEMBLER_NAME))
+        # sys.stderr.write('[%s wrapper] Started installation of %s.\n' % (ASSEMBLER_NAME, ASSEMBLER_NAME))
+        log('Started installation of %s.' % (ASSEMBLER_NAME), None);
         if (not os.path.exists(ASSEMBLERS_PATH_ROOT_ABS)):
-            sys.stderr.write('[%s wrapper] Creating a directory on path "%s".\n' % (ASSEMBLER_NAME, ASSEMBLERS_PATH_ROOT_ABS))
+            # sys.stderr.write('[%s wrapper] Creating a directory on path "%s".\n' % (ASSEMBLER_NAME, ASSEMBLERS_PATH_ROOT_ABS))
+            log('Creating a directory on path "%s".' % (ASSEMBLERS_PATH_ROOT_ABS), None);
             os.makedirs(ASSEMBLERS_PATH_ROOT_ABS);
 
         command = 'cd %s; git clone %s' % (ASSEMBLERS_PATH_ROOT_ABS, ASSEMBLER_URL)
-        execute_command(command, dry_run=DRY_RUN);
+        execute_command(command, None, dry_run=DRY_RUN);
 
         setup_commands = [];
         setup_commands.append('cd %s' % (ASSEMBLER_PATH));
@@ -207,7 +369,7 @@ def download_and_install():
         setup_commands.append('make');
         setup_commands.append('cp daligner daligner_p DB2Falcon HPCdaligner LA4Falcon LAmerge LAsort  $FC/bin');
         command = '; '.join(setup_commands);
-        execute_command(command, dry_run=DRY_RUN);
+        execute_command(command, None, dry_run=DRY_RUN);
 
 def verbose_usage_and_exit():
     sys.stderr.write('Usage:\n')
