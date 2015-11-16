2b4473aa8a9f47137a38b68fd190e14cf38a0f2a
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2015-11-16 10:41:29 +0100

    Fixed the problem with preparing the data for ALLPATHS-LG.

--- src/wrappers/wrapper_allpathslg.py
+++ src/wrappers/wrapper_allpathslg.py
@@ -349,12 +349,14 @@ def run(datasets, output_path):
         log('Creating a directory on path "%s".' % (data_dir), None);
         os.makedirs(data_dir);
 
-    command = '%s %s/bin/PrepareAllPathsInputs.pl DATA_DIR=%s PLOIDY=1 IN_GROUPS_CSV=%s IN_LIBS_CSV=%s PICARD_TOOLS_DIR=%s OVERWRITE=True | tee %s' % \
+    ### ALLPATHS-LG crashes when it cannot parse a double value from a string, e.g. "101.0" would cause a crash if the locale is not set to US. Set correct decimal separator!! export LC_NUMERIC='en_US.utf8'
+    command = 'export LC_NUMERIC=\'en_US.utf8\'; %s %s/bin/PrepareAllPathsInputs.pl DATA_DIR=%s PLOIDY=1 IN_GROUPS_CSV=%s IN_LIBS_CSV=%s PICARD_TOOLS_DIR=%s OVERWRITE=True | tee %s' % \
                 (measure_command('%s-%d.memtime' % (ASSEMBLER_NAME, num_memtimes)), ASSEMBLER_PATH, data_dir, in_groups_csv_path, in_libs_csv_path, PICARDTOOLS_PATH, 'prepare.out');
     execute_command(command, fp_log, dry_run=DRY_RUN);
 
-    num_memtimes = 0;
-    command = '%s %s PRE=%s DATA_SUBDIR=data_test REFERENCE_NAME=data THREADS=%d RUN=run TARGETS=standard OVERWRITE=True | tee -a %s/assemble_test.out' % \
+    num_memtimes += 1;
+    ### ALLPATHS-LG crashes when it cannot parse a double value from a string, e.g. "101.0" would cause a crash if the locale is not set to US. Set correct decimal separator!! export LC_NUMERIC='en_US.utf8'
+    command = 'export LC_NUMERIC=\'en_US.utf8\'; %s %s PRE=%s DATA_SUBDIR=data_test REFERENCE_NAME=data THREADS=%d RUN=run TARGETS=standard OVERWRITE=True | tee -a %s/assemble_test.out' % \
                 (measure_command('%s-%d.memtime' % (ASSEMBLER_NAME, num_memtimes)), ASSEMBLER_BIN, output_path, num_threads, output_path);
     execute_command(command, fp_log, dry_run=DRY_RUN);
 
