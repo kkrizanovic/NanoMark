978d2601ac78c42b2c909cb0455e681f2b28401c
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2015-11-18 22:54:18 +0100

    Working on the main framework script.

--- src/nanomark.py
+++ src/nanomark.py
@@ -117,7 +117,7 @@ def benchmark(reads_file, reference_file, technology, wrapper_list = []):
             create_output_folder = True
             wrapper = 'wrapper_' + wrapper_basename
             wrapper_path = os.path.join(basicdefines.WRAPPERS_PATH_ROOT_ABS, wrapper + '.py')
-            command = 'import %s; assembler_name = %s.ASSEMBLER_NAME; results_file = %s.ASSEMBLER_RESULTS; create_output_folder = %s.CREATE_OUTPUT_FOLDER; assembler_type = %s.ASSEMBLER_TYPE' % (wrapper, wrapper, wrapper, wrapper, wrapper)
+            command = 'import %s; assembler_name = %s.ASSEMBLER_NAME; create_output_folder = %s.CREATE_OUTPUT_FOLDER; assembler_type = %s.ASSEMBLER_TYPE' % (wrapper, wrapper, wrapper, wrapper, wrapper)
             sys.stderr.write('Executing command: %s\n' % (command));
             exec(command)
             # Run assembly only using non-hybrid assemblers. Using hybrid assemblers complicates uniform specification of the datasets.
@@ -192,7 +192,7 @@ def continueBenchmark(results_folder):
             create_output_folder = True
             wrapper = 'wrapper_' + wrapper_basename
             wrapper_path = os.path.join(basicdefines.WRAPPERS_PATH_ROOT_ABS, wrapper + '.py')
-            command = 'import %s; assembler_name = %s.ASSEMBLER_NAME; results_file = %s.ASSEMBLER_RESULTS; create_output_folder = %s.CREATE_OUTPUT_FOLDER; assembler_type = %s.ASSEMBLER_TYPE' % (wrapper, wrapper, wrapper, wrapper, wrapper)
+            command = 'import %s; assembler_name = %s.ASSEMBLER_NAME; create_output_folder = %s.CREATE_OUTPUT_FOLDER; assembler_type = %s.ASSEMBLER_TYPE' % (wrapper, wrapper, wrapper, wrapper, wrapper)
             exec(command)
             # Run assembly only using non-hybrid assemblers. Using hybrid assemblers complicates uniform specification of the datasets.
             if (assembler_type != 'nonhybrid'):
