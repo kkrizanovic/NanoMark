56e18bc4a3842567c96a8d1fcf74d52f517a1cf3
Ivan Sovic (T530) <ivan.sovic@gmail.com>
2015-11-19 15:53:23 +0100

    Working on the main framework script.

--- src/nanomark.py
+++ src/nanomark.py
@@ -295,7 +295,7 @@ def summarize_results(results_folder):
                     sys.stderr.write('Running analyses on the assembly output.\n');
                     basicdefines.execute_command('mkdir -p %s' % (quast_out_folder), None);
                     basicdefines.execute_command('mkdir -p %s' % (dnadiff_out_folder), None);
-                    basicdefines.execute_command('%s %s -R %s -o %s' % (basicdefines.QUAST_BIN, assembly_path, reference_file, dnadiff_out_folder), None);
+                    basicdefines.execute_command('%s %s -R %s -o %s' % (basicdefines.QUAST_BIN, assembly_path, reference_file, quast_out_folder), None);
                     basicdefines.execute_command('%s %s %s -p %s/out' % (basicdefines.DNADIFF_BIN, reference_file, assembly_path, dnadiff_out_folder), None);
                 pass;
             else:   # In this case, the contig file does not exist.
